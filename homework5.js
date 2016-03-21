71b9555f28aa1d53923e3964f69b8a0493f7a3c1
j-clay <jclay92@gmail.com>
2016-03-20 19:40:16 -0700

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -8,28 +7,35 @@ var loss = 0;
 app.use(bodyParser.json());
 
 app.get('/stats', function(req, res) {
-	res.sendStatus(JSON.stringify({"wins":wins, "losses":loss}));
-	});
+    res.sendStatus(JSON.stringify({
+        "wins": wins,
+        "losses": loss
+    }));
+});
 
 app.post('/flip', function(req, res) {
-	var value = req.body.call;
-	var rand_num = Math.floor((Math.random() * 2) + 1);
-	var s_value = get_side(rand_num);
+    var value = req.body.call;
+    var rand_num = Math.floor((Math.random() * 2) + 1);
+    var s_value = get_side(rand_num);
 
-	if (value == s_value) {
-		res.end(JSON.stringify({"result": "win"}));
-		wins++;
-	} else {
-		res.end(JSON.stringify({"result":"loss"}));
-		loss++;
-	}
+    if (value == s_value) {
+        res.end(JSON.stringify({
+            "result": "win"
+        }));
+        wins++;
+    } else {
+        res.end(JSON.stringify({
+            "result": "loss"
+        }));
+        loss++;
+    }
 });
-app.listen(8080, function () {
-	console.log('App running on port 8080!');
+app.listen(8080, function() {
+    console.log('App running on port 8080!');
 });
 
 function get_side(val) {
-	if (val == 1) return "heads";
-	else return "tails";
+    if (val == 1) return "heads";
+    else return "tails";
 
 }
